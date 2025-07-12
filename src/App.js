e183d254a63a8f200c0473b7928407cced607470
Victor Bundi <victorbundi.dev@gmail.com>
2025-07-12 16:50:01 +0300

    localstorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Tasks from './Tasks';
 import AddTask from './AddTask';
@@ -9,6 +9,10 @@ Store tasks in localStorage.*/}
 function App() {
   const [todos, setTodos] = useState([]);
   const [task, setTask] = useState("");
+
+  const storeTasks = (todos) => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }
 
   const addTodo = (todo) => {
     const newTodos = [...todos, todo];
@@ -34,6 +38,13 @@ function App() {
     ...todos.slice(index + 1)];
     setTodos(newTodos);
   }
+
+  useEffect(() => {
+    storeTasks(todos);
+    console.log("Called");
+    const retrieved = JSON.parse(localStorage.getItem('todos'));
+    console.log(retrieved);
+  }, [todos]);
 
   return (
     <div className='App'>
--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import EditTask from "./EditTask";
 
 function Task(props) {
